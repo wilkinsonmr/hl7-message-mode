13ea9afde89f6bcc37e6689aca01341f5c8e1b82
Mark Wilkinson <wilkinsonmr@gmail.com>
2022-01-30 13:41:21 -0600

    Initial package skeleton

---
+++ hl7-message.el
@@ -0,0 +1,20 @@
+;;; hl7-message.el --- View and navigate HL7 message files
+
+;; Copyright (C) 2022 Mark R. Wilkinson
+
+;; Author: Mark R. Wilkinson <wilkinsonmr@gmail.com>
+;; Version: 1.0
+;; Package-Requires: ((cl-lib) (tree-mode) (hierarchy "0.7.0"))
+;; Keywords: HL7
+;; URL: https://github.com/wilkinsonmr/hl7-message-mode
+
+
+;;; Commentary:
+
+;; This package provides a major mode to view and navigate HL7 messages.
+
+;;; Code:
+
+(provide 'hl7-message)
+
+;;; hl7-message.el ends here
